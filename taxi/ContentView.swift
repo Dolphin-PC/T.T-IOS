aff5838a5d8df89880c4595347e252b55efab2d1
park chanyoung <pcx474@gmail.com>
2020-02-08 15:54:28 +0900

    Initial Commit

---
+++ taxi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  taxi
+//
+//  Created by 박찬영 on 2020/02/08.
+//  Copyright © 2020 박찬영. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
